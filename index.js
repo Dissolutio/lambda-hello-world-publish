49eace562f04d3c7af633366234a4b147e90ec18
John <entity.john@gmail.com>
2022-05-24 13:07:41 -0500

    Update index.js
    now it sends you an email

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
-console.log('Loading function');
+var aws = require("aws-sdk");
+var ses = new aws.SES({ region: "YOUR_SES_AWS_REGION" });
 
-exports.handler = (event, context, callback) => {
-    console.log('Received event:', JSON.stringify(event, null, 2));
-    console.log('value1 =', event.key1);
-    console.log('value2 =', event.key2);
+exports.handler = async function (event, context, callback) {
+    console.log('Received event:', JSON.stringify(event, null, 4));
+    var message = event.Records[0].Sns.Message;
+    var parsedMessage = {};
+    try {
+        parsedMessage = JSON.parse(message);
+        console.log('parsedMessage:', parsedMessage);
+    } catch (e) {
+        console.error('Could not parse message as JSON! message:', message);
+    }
+
+    try {
+        var rawMessage = parsedMessage?.message ?? 'No parsed message, so you get this instead: <3';
+        var emailSubject = 'Hello world from your lambda';
+
+    } catch (e) {
+        console.error('Failed to initialize variables:', message);
+        return;
+    }
+
+    var params = {
+        Destination: {
+            ToAddresses: ["YOUR_SES_VERIFIED_EMAIL_ADDRESS_TO_SEND_TO"],
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: "UTF-8",
+                    Data: rawMessage
+                },
+                Text: {
+                    Charset: "UTF-8",
+                    Data: rawMessage
+                }
+            },
+            Subject: {
+                Charset: "UTF-8",
+                Data: emailSubject
+            }
+        },
+        Source: "YOUR_SES_VERIFIED_EMAIL_ADDRESS_TO_SEND_FROM",
+    };
+
+    var sendEmailPromise = ses.sendEmail(params).promise();
+    sendEmailPromise.then(function (data) {
+        console.log('Email Success!', data);
+    }).catch(function (err) {
+        console.log('Email failed', err);
+    });
+    return sendEmailPromise;
 };
